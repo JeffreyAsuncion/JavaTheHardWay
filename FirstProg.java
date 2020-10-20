1ed44d2b04fdf11c9afe64fb2f922109d0b170eb
JeffreyAsuncion <jeffrey.l.asuncion@gmail.com>
2020-10-20 12:27:43 -0400

    hello world java!!!

---
+++ FirstProg.java
@@ -0,0 +1,7 @@
+public class FirstProg {
+    public static void main( String[] args ) {
+	System.out.println("Hello World!!!");
+    	System.out.println("I, Jeffrey Asuncion, am determined to learn how to code.");
+	System.out.println("Today's date is Tuesday , October 20, 2020.");
+    }
+}
